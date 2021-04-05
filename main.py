d3a813f811629f39f2723dd56c6b184f8ca76424
Laysan Salimgaraeva <fluoritecaf2@mail.ru>
2021-04-05 18:01:04 +0300

    def realised

--- main.py
+++ main.py
@@ -3,20 +3,32 @@ with open('recipes.txt', encoding='utf-8') as f:
     while True:
         dish_name = f.readline().strip()
         count = int(f.readline())
+        ing_list = []
         for el in range(count):
             ingr = f.readline().strip()
             splited = ingr.split('|')
-            ing_dict = {'ingredient_name': splited[0], 'quantity': splited[1], 'measure': splited[2]}
-        cook_book[dish_name] = ing_dict
+            ing_list.append({'ingredient_name': splited[0], 'quantity': splited[1], 'measure': splited[2]})
+        cook_book[dish_name] = ing_list
         line = f.readline()
         if not line:
             break
 f.close()
-# print(cook_book)
-# def get_shop_list_by_dishes(dishes, person_count):
-#     for el in dishes:
-#         for cook_book.keys() in cook_book:
-#             if el =
 
 
-get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+def get_shop_list_by_dishes(dishes, person_count):
+    set_1 = set(cook_book.keys()) and set(dishes)
+    shop_list = {}
+    for el in set_1:
+        x = 0
+        list_ = cook_book.get(el)
+        for elem in list_:
+            if list_[x].get('ingredient_name') in shop_list:
+                shop_list[list_[x].get('ingredient_name')]['quantity'] += int(list_[x].get('quantity')) * person_count
+            else:
+                shop_list[list_[x].get('ingredient_name')] = {'measure': list_[x].get('measure'),
+                                                              'quantity': int(list_[x].get('quantity')) * person_count}
+                x += 1
+    print(shop_list)
+
+
+get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2)
